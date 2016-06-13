2cb6b8825658c49e47cec9dd31891f49f6bee083
lubegasylvia <lubegasylvia@gmail.com>
2016-06-13 20:40:13 +0200

    Adding search box and some users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
+
 User.create({ name: "Santa", email: "santa@hotmail.com", password: "reindeer"})
+User.create({ name: "Jane", email: "janeB@gmail.net", password: "atjaneb"})
+User.create({ name: "Phiona", email: "phiona@hyahoo.com", password: "38phio"})
+User.create({ name: "Suzan", email: "zuz@hotmail.com", password: "suzan123"})
+User.create({ name: "Paddy", email: "padyD@gmail.com", password: "pad78383"})
+User.create({ name: "David", email: "david@hotmail.net", password: "7e99e"})
 
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
