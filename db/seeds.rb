1ac39759506a92902481894d697726a29a0d1ff3
YOUR NAME <YOUR@EMAIL.com>
2016-06-13 19:04:33 +0530

    toilet seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 User.create({ name: "Santa", email: "santa@hotmail.com", password: "reindeer"})
 
+Toilet.create({ name: "Regular", price: "5.20", cleanliness: "Pretty clean", user:references})
+Toilet.create({ name: "Little comfort", price: "3.50", cleanliness: "Not very clean", user:references})
+Toilet.create({ name: "Sit and relax", price: "9.36", cleanliness: "Very clean", user:references})
+Toilet.create({ name: "The Golden Throne", price: "13.21", cleanliness: "Extremely clean", user:references})
+Toilet.create({ name: "Hold your breath", price: "1.93", cleanliness: "Dirty", user:references})
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
